d477f6614634031c11ddb17de387eb32fd077ebe
perryharlock <perry.harlock@tiscali.co.uk>
2013-09-24 14:33:07 +0100

    Add route for json download

--- route/task.js
+++ route/task.js
@@ -31,4 +31,19 @@ function route (app) {
 			});
 		});
 	});
+
+	app.express.get('/:id.json', function (req, res, next) {
+		app.webservice.task(req.params.id).get({}, function (err, task) {
+			if (err) {
+				return next();
+			}
+			app.webservice.task(req.params.id).results({}, function (err, results) {
+				if (err) {
+					return next(err);
+				}
+				task.results = results;
+				res.send(task);
+			});
+		});
+	});
 }
