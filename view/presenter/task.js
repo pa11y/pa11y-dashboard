76ad3d19afbae807305a9c9171e817bbb685be4b
Rowan Manning <rowan.manning@nature.com>
2013-09-27 09:20:11 +0100

    Add descriptions to ignore rules

--- view/presenter/task.js
+++ view/presenter/task.js
@@ -1,6 +1,10 @@
 'use strict';
 
+var _ = require('underscore');
 var presentResult = require('./result');
+var standardsArray = require('../../data/standards')();
+var rules = createStandardDescriptionMap(standardsArray);
+console.log(rules);
 
 module.exports = presentTask;
 
@@ -11,6 +15,14 @@ function presentTask (task) {
     task.hrefDelete = '/' + task.id + '/delete';
     task.hrefJson = '/' + task.id + '.json';
 
+    // Enhance the ignored rules
+    task.ignore = task.ignore.map(function (name) {
+        return {
+            name: name,
+            description: rules[name]
+        };
+    });
+
     // Present the last result if present
     if (task.last_result) {
         task.lastResult = presentResult(task.last_result);
@@ -18,4 +30,14 @@ function presentTask (task) {
     }
 
     return task;
+}
+
+function createStandardDescriptionMap (standards) {
+    var map = {};
+    standards.forEach(function (standard) {
+        standard.rules.forEach(function (rule) {
+            map[rule.name] = rule.description;
+        });
+    });
+    return map;
 }
