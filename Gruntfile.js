3bc91069426d3fb629f2d86e14f73a9bf8c2341a
Rowan Manning <rowan.manning@nature.com>
2013-11-05 11:53:46 +0000

    Minify the site JavaScript

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,10 +29,35 @@ module.exports = function (grunt) {
 			}
 		},
 
+		uglify: {
+			options: {
+				mangle: false
+			},
+			all: {
+				files: {
+					'public/js/site.min.js': [
+						'public/js/vendor/jquery/jquery.min.js',
+						'public/js/vendor/bootstrap/js/alert.js',
+						'public/js/vendor/bootstrap/js/dropdown.js',
+						'public/js/vendor/bootstrap/js/tooltip.js',
+						'public/js/vendor/flot/jquery.flot.js',
+						'public/js/vendor/flot/jquery.flot.categories.js',
+						'public/js/vendor/flot/jquery.flot.selection.js',
+						'public/js/vendor/flot/jquery.flot.resize.js',
+						'public/js/site.js'
+					]
+				}
+			}
+		},
+
 		watch: {
 			less: {
 				files: ['public/less/**/*.less'],
 				tasks: ['less']
+			},
+			js: {
+				files: ['public/js/**/*.js', '!public/js/site.min.js'],
+				tasks: ['uglify']
 			}
 		}
 
@@ -40,10 +65,11 @@ module.exports = function (grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('lint', ['jshint']);
-	grunt.registerTask('compile', ['less']);
+	grunt.registerTask('compile', ['less', 'uglify']);
 	grunt.registerTask('default', ['compile', 'lint']);
 	grunt.registerTask('ci', ['compile', 'lint']);
 
