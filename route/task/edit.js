963ab33cce819cbc19224883c92e542ad1a43325
perryharlock <perry.harlock@tiscali.co.uk>
2013-11-27 16:21:18 +0000

    Make edit route work and add options dropdown

--- route/task/edit.js
+++ route/task/edit.js
@@ -2,6 +2,7 @@
 
 var _ = require('underscore');
 var presentTask = require('../../view/presenter/task');
+var getStandards = require('../../data/standards');
 
 module.exports = route;
 
@@ -13,7 +14,21 @@ function route (app) {
 			if (err) {
 				return next();
 			}
+			var standards = getStandards().map(function (standard) {
+				if (standard.title === task.standard) {
+					standard.selected = true;
+				}
+				standard.rules = standard.rules.map(function (rule) {
+					if (task.ignore.indexOf(rule.name) !== -1) {
+						rule.ignored = true;
+					}
+					return rule;
+				});
+				return standard;
+			});
 			res.render('task/edit', {
+				edited: (typeof req.query.edited !== 'undefined'),
+				standards: standards,
 				task: presentTask(task),
 				isTaskPage: true
 			});
@@ -21,12 +36,7 @@ function route (app) {
 	});
 
 	app.express.post('/:id/edit', function (req, res, next) {
-		app.webservice.task(req.params.id).remove(function (err) {
-			if (err) {
-				return next();
-			}
-			res.redirect('index');
-		});
+		res.redirect('/' + req.params.id + '/edit?edited');
 	});
 
 }
