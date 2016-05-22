c15cc32542798f449e89ab6b2f070325c4aecb9c
Rowan Manning <info@rowanmanning.com>
2016-05-22 13:05:08 +0100

    Fix some lint errors

--- config.js
+++ config.js
@@ -13,27 +13,29 @@
 // You should have received a copy of the GNU General Public License
 // along with pa11y-dashboard.  If not, see <http://www.gnu.org/licenses/>.
 
+'use strict';
+
 var fs = require('fs');
 var jsonPath = './config/' + (process.env.NODE_ENV || 'development') + '.json';
 
 if (fs.existsSync(jsonPath)) {
-  module.exports = require(jsonPath);
+	module.exports = require(jsonPath);
 } else {
-  module.exports = {
-    port: Number(env('PORT', '4000')),
-    noindex: env('NOINDEX', 'true') == 'true',
-    readonly: env('READONLY', 'false') == 'true',
+	module.exports = {
+		port: Number(env('PORT', '4000')),
+		noindex: env('NOINDEX', 'true') === 'true',
+		readonly: env('READONLY', 'false') === 'true',
 
-    webservice: env('WEBSERVICE_URL', {
-      database: env('WEBSERVICE_DATABASE', 'mongodb://localhost/pa11y-webservice'),
-      host: env('WEBSERVICE_HOST', '0.0.0.0'),
-      port: Number(env('WEBSERVICE_PORT', '3000')),
-      cron: env('WEBSERVICE_CRON', false)
-    })
-  }
+		webservice: env('WEBSERVICE_URL', {
+			database: env('WEBSERVICE_DATABASE', 'mongodb://localhost/pa11y-webservice'),
+			host: env('WEBSERVICE_HOST', '0.0.0.0'),
+			port: Number(env('WEBSERVICE_PORT', '3000')),
+			cron: env('WEBSERVICE_CRON', false)
+		})
+	};
 }
 
 function env(name, defaultValue) {
-  var value = process.env[name];
-  return typeof value == 'string' ? value : defaultValue;
+	var value = process.env[name];
+	return typeof value === 'string' ? value : defaultValue;
 }
