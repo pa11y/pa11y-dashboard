5b3d52c5cb27cee38d609232987edce49478620d
Brendan Connelly <73497071+bconnelly-niche@users.noreply.github.com>
2021-05-03 14:49:19 -0700

    add successful POST requests of tasks within the idArray loop

--- view/custom_functions/runAllTasks.js
+++ view/custom_functions/runAllTasks.js
@@ -1,6 +1,7 @@
 /* eslint-disable id-blacklist */
 'use strict';
 
+const fetch = require('node-fetch');
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost/pa11y-webservice-dev';
 
@@ -15,13 +16,27 @@ MongoClient.connect(url, (err, db) => {
 		if (err) {
 			throw err;
 		}
-		console.log(result);
+		db.close();
+		// console.log(result);
 		// eslint-disable-next-line no-shadow
 		const idArray = result.map(result => {
 			// eslint-disable-next-line no-underscore-dangle
 			return result._id;
 		});
+		console.log('Array of task Ids: \n');
 		console.log(idArray);
-		db.close();
+
+		// eslint-disable-next-line no-plusplus
+		for (let i = 0; i < idArray.length; i++) {
+			console.log(`${i} : ${idArray[i]}`);
+
+			const postUrl = `http://0.0.0.0:3999/tasks/${idArray[i]}/run`;
+			fetch(postUrl, {
+				method: 'POST'
+			}).then(res => {
+				// eslint-disable-next-line max-len
+				console.log(`${idArray[i]} task successfully run! Request #${i} / ${idArray.length}`);
+			});
+		}
 	});
 });
