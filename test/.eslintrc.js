edabfa1bba4a55616db3c14876f905e9918afc62
Danyal Aytekin <danyal@alienpaper.com>
2023-11-13 17:24:12 +0000

    Update to `pa11y-lint-config@3`, update `ecmaVersion` to 2020, remove some rule overrides

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,15 +1,9 @@
 'use strict';
 
-// Clone the main config
-const config = module.exports = JSON.parse(JSON.stringify(require('../.eslintrc')));
+const config = require('../.eslintrc');
 
 // We use `this` all over the integration tests
 config.rules['no-invalid-this'] = 'off';
-
-// Because of our use of `this`, arrow functions
-// aren't really gonna work in the integration tests
 config.rules['prefer-arrow-callback'] = 'off';
 
-// Disable max line length/statements
-config.rules['max-len'] = 'off';
-config.rules['max-statements'] = 'off';
+module.exports = config;
