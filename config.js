8253a12e6b24c0f514f2f948d079962ddcbcd65d
Jose Bolos <josebolos@users.noreply.github.com>
2026-03-06 15:54:09 +0000

    Add support for environment variables for Chrome config
    This improves the 12FA compliance of the app as the Chrome config is going to be required in almost every modern environment.

--- config.js
+++ config.js
@@ -25,16 +25,30 @@ if (fs.existsSync(jsonPath)) {
 } else if (fs.existsSync(jsPath)) {
 	module.exports = require(jsPath);
 } else {
+	const webserviceConfig = {
+		database: env('WEBSERVICE_DATABASE', 'mongodb://localhost/pa11y-webservice'),
+		host: env('WEBSERVICE_HOST', '0.0.0.0'),
+		port: Number(env('WEBSERVICE_PORT', '3000')),
+		cron: env('WEBSERVICE_CRON', false)
+	};
+
+	const chromiumFlags = env('CHROMIUM_FLAGS', undefined);
+	const chromiumExecutable = env('CHROMIUM_EXECUTABLE', undefined);
+	if (chromiumFlags || chromiumExecutable) {
+		webserviceConfig.chromeLaunchConfig = {};
+		if (chromiumFlags) {
+			webserviceConfig.chromeLaunchConfig.args = chromiumFlags.split(',');
+		}
+		if (chromiumExecutable) {
+			webserviceConfig.chromeLaunchConfig.executablePath = chromiumExecutable;
+		}
+	}
+
 	module.exports = {
 		port: Number(env('PORT', '4000')),
 		noindex: env('NOINDEX', 'true') === 'true',
 		readonly: env('READONLY', 'false') === 'true',
-		webservice: env('WEBSERVICE_URL', {
-			database: env('WEBSERVICE_DATABASE', 'mongodb://localhost/pa11y-webservice'),
-			host: env('WEBSERVICE_HOST', '0.0.0.0'),
-			port: Number(env('WEBSERVICE_PORT', '3000')),
-			cron: env('WEBSERVICE_CRON', false)
-		})
+		webservice: env('WEBSERVICE_URL', webserviceConfig)
 	};
 }
 
